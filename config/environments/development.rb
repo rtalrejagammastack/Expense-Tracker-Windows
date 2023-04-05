a53ff70369f9d06d83dc6f32f781d7e5fe4ccf18
rtalrejagammastack <rtalreja@grepruby.io>
2023-04-05 12:19:58 +0530

    Confirmable Added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,8 +72,20 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_cable.disable_request_forgery_protection = true\
+  
+  config.action_mailer.default_url_options = { host: ENV["MAIL_DOMAIN"], port: ENV["MAIL_PORT"] }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV["MAIL_USERNAME"],
+    password:       ENV["MAIL_PASSWORD"],
+    domain:         ENV["MAIL_DOMAIN"],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
     html_tag.html_safe
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_02_113322) do
+ActiveRecord::Schema.define(version: 2023_04_05_052915) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
