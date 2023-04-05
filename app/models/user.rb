3f4c6434fbf0babf5dc3842d8d4ff824cd0d037a
Rohit Talreja <=>
2023-04-05 08:38:51 +0530

    Devise Integration started

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable
+          # :omniauthable
 
   acts_as_paranoid column: :destroyed_at
 
